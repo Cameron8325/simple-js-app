5c8b6e35d3d2b05063550b61d6f7f793994aab41
Cameron8325 <140201247+Cameron8325@users.noreply.github.com>
2023-08-16 15:07:50 -0400

    Updated Heights, Added For Loop, Added Conditional Height Message
    Saved as one big change, will commit more individually in future. I updated the heights to full values, I added a for loop to list the Pokemon and their heights, then used a conditional to print a special message based on height parameters.

--- js/scripts.js
+++ js/scripts.js
@@ -3,24 +3,45 @@ let pokemonList = []
 pokemonList = [
   {
     name: 'Bulbasaur',
-    height: 7,
+    height: 0.7,
     types: ["grass", "poison"]
 },
 
   {
     name: 'Charmander',
-    height: 6,
+    height: 0.6,
     types: ["fire"]
 },
 
   {
     name: 'Squirtle',
-    height: 5,
+    height: 0.5,
     types: ["water"]
 },
   {
     name: 'Caterpie',
-    height: 3,
+    height: 0.3,
     type: ["bug"]
+  },
+  {
+    name: 'Charizard',
+    height: 1.7,
+    type: ["fire", "flying"]
   }
 ]
+
+for (let i = 0; i < pokemonList.length; i++){
+  let pokemonName = pokemonList[i].name;
+  let pokemonHeight = pokemonList[i].height;
+  let heightMessage = "";
+
+  if (pokemonHeight > 1.6) {
+    heightMessage = "Wow, that's so big!";
+  } else if (pokemonHeight > 0.3 && pokemonHeight < 1.6) {
+    heightMessage = "Average Sized Pokemon";
+  } else {
+    heightMessage = "Small Pokemon";
+  }
+
+  document.write(pokemonName + " (height: " + pokemonHeight + ") - " + heightMessage + "<br>");
+}
