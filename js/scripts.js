d4d684a3d5a5294507060498aee888bebda91064
Cameron8325 <140201247+Cameron8325@users.noreply.github.com>
2023-08-22 16:37:38 -0400

    Wrapped pokemonList in IIFE
    Wrapped pokemonList in IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,53 @@
-let pokemonList = []
+let pokemonRepository = (function() {
+  let pokemonList = []
 
-pokemonList = [
-  {
-    name: 'Bulbasaur',
-    height: 0.7,
-    types: ["grass", "poison"]
-},
-
-  {
-    name: 'Charmander',
-    height: 0.6,
-    types: ["fire"]
-},
-
-  {
-    name: 'Squirtle',
-    height: 0.5,
-    types: ["water"]
-},
-  {
-    name: 'Caterpie',
-    height: 0.3,
-    type: ["bug"]
+  pokemonList = [
+    {
+      name: 'Bulbasaur',
+      height: 0.7,
+      types: ["grass", "poison"]
   },
-  {
-    name: 'Charizard',
-    height: 1.7,
-    type: ["fire", "flying"]
-  }
-]
 
-pokemonList.forEach(function(pokemon){
-  let pokemonName = pokemonList[i].name;
-  let pokemonHeight = pokemonList[i].height;
+    {
+      name: 'Charmander',
+      height: 0.6,
+      types: ["fire"]
+  },
+
+    {
+      name: 'Squirtle',
+      height: 0.5,
+      types: ["water"]
+  },
+    {
+      name: 'Caterpie',
+      height: 0.3,
+      type: ["bug"]
+    },
+    {
+      name: 'Charizard',
+      height: 1.7,
+      type: ["fire", "flying"]
+    }
+  ];
+
+  function getALL() {
+    return pokemonList;
+  }
+
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  return {
+    getALL: getALL,
+    add: add
+  };
+  })();
+
+pokemonRepository.getALL().forEach(function(pokemon){
+  let pokemonName = pokemonL.name;
+  let pokemonHeight = pokemon.height;
   let heightMessage = "";
 
   if (pokemonHeight > 1.6) {
