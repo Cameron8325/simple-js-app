bc47df48de9e43f0eabf94aee04e2b6c63704e58
Cameron8325 <140201247+Cameron8325@users.noreply.github.com>
2023-08-27 16:48:34 -0400

    Added function and event listener.

--- js/scripts.js
+++ js/scripts.js
@@ -45,8 +45,15 @@ let pokemonRepository = (function() {
     let button = document.createElement("button");
     button.innerText = pokemon.name;
     button.classList.add("button-class");
+    button.addEventListener('click', function () {
+      showDetails(pokemon)
+    });
     listPokemon.appendChild(button);
     pokemonList.appendChild(listPokemon)
+  }
+
+  function showDetails (pokemon) {
+    console.log(pokemon)
   }
 
   return {
