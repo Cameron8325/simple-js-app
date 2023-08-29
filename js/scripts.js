5d797473e022c256c96ecd20936279ce572ebe4f
Cameron8325 <140201247+Cameron8325@users.noreply.github.com>
2023-08-29 12:11:35 -0400

    Added API

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,6 @@
 let pokemonRepository = (function() {
-  let repository = []
-
-  pokemonList = [
-    {
-      name: 'Bulbasaur',
-      height: 0.7,
-      types: ["grass", "poison"]
-  },
-
-    {
-      name: 'Charmander',
-      height: 0.6,
-      types: ["fire"]
-  },
-
-    {
-      name: 'Squirtle',
-      height: 0.5,
-      types: ["water"]
-  },
-    {
-      name: 'Caterpie',
-      height: 0.3,
-      type: ["bug"]
-    },
-    {
-      name: 'Charizard',
-      height: 1.7,
-      type: ["fire", "flying"]
-    }
-  ];
+  let pokemonList = [];
+  let apiURL = 'https://pokeapi.co/api/v2/pokemon/?limit=1281'
 
   function getALL() {
     return pokemonList;
