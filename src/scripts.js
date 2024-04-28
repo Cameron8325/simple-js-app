6dc1ea2796b054a7be01fd5ce75083f4e86f3ffa
Cameron8325 <140201247+Cameron8325@users.noreply.github.com>
2024-04-28 17:52:15 -0400

    Restyled and Reminified

--- src/scripts.js
+++ src/scripts.js
@@ -14,10 +14,10 @@ let pokemonRepository = (function () {
     let pokemonList = document.querySelector(".pokemon-list");
     let listPokemon = document.createElement("li");
     let button = document.createElement("button");
-    listPokemon.classList.add("list-group-item", "mx-auto");
-    button.innerText = pokemon.name;
+    listPokemon.classList.add("list-group-item");
+    button.innerText = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
 
-    button.classList.add("btn", "btn-primary", "btn-lg");
+    button.classList.add("btn-primary", "btn-lg");
     button.setAttribute('data-target', '#exampleModal');
     button.setAttribute('data-toggle', 'modal');
     listPokemon.appendChild(button);
@@ -30,20 +30,28 @@ let pokemonRepository = (function () {
   function showModal(pokemon) {
     let modalBody = $('.modal-body');
     let modalTitle = $('.modal-title');
-    let modalHeader = $('.modal-header');
 
     // clear existing content of the modal
     modalTitle.empty();
     modalBody.empty();
 
     // creating element for name in modal content
-    let nameElement = $('<h5 class="modal-title">' + pokemon.name + '</h5>');
+    let nameElement = $('<h5 class="modal-title">' + pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1) + '</h5>');
     let imageElement = $('<img class="modal-img">');
       imageElement.attr('src', pokemon.imageUrl);
-    let heightElement = $('<p>' + 'height : ' + pokemon.height + ' decimeters' + '</p>');
-    let weightElement = $('<p>' + 'weight : ' + pokemon.weight + ' decagrams' + '</p>');
-    let typesElement = $('<p>' + 'Types: ' + pokemon.types.join(', ') + '</p>');
-    let abilitiesElement = $('<p>' + 'Abilities: ' + pokemon.abilities.join(', ') + '</p>');
+    let heightElement = $('<p>' + 'Height : ' + pokemon.height + ' Decimeters' + '</p>');
+    let weightElement = $('<p>' + 'Weight : ' + pokemon.weight + ' Decagrams' + '</p>');
+     // Capitalize the first letter of each type
+     let typesCapitalized = pokemon.types.map(function(type) {
+      return type.charAt(0).toUpperCase() + type.slice(1);
+  });
+  let typesElement = $('<p>' + 'Types: ' + typesCapitalized.join(', ') + '</p>');
+
+  // Capitalize the first letter of each ability
+  let abilitiesCapitalized = pokemon.abilities.map(function(ability) {
+      return ability.charAt(0).toUpperCase() + ability.slice(1);
+  });
+  let abilitiesElement = $('<p>' + 'Abilities: ' + abilitiesCapitalized.join(', ') + '</p>');
 
 
     modalTitle.append(nameElement);
